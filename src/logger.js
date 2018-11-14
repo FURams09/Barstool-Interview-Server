6f02f697abd6c89a17f3888297a41b3204adf57d
FURams09 <padin.greg@gmail.com>
2018-11-14 18:21:31 -0500

    Commenting logger.js
    Added an explanation of what the logging library does.

--- src/logger.js
+++ src/logger.js
@@ -1,3 +1,12 @@
+
+/**
+* Add any custom logging functionality here. 
+* for simplicity I used this to log any handled 
+* exception and then return a standard error object
+* as opposed to just returning false or throwing to an
+* error boundary
+*
+*/
 module.exports = {
   log: errMsg => {
     console.log(errMsg)
