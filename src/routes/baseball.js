4f003e395a74cbc0c053535f41ef88559c94e43c
FURams09 <padin.greg@gmail.com>
2018-11-14 19:00:17 -0500

    Update baseball.js
    Cleaned up comments in router for baseball

--- src/routes/baseball.js
+++ src/routes/baseball.js
@@ -1,15 +1,17 @@
 const express = require(`express`)
 const router = express.Router()
 
-const Logger = require(`../logger`)
 const { RefreshData, GetGameFeed } = require(`../utility`)
-
 const BaseballGame = require(`../db/baseball-game`)
+const Logger = require(`../logger`)
 
 /**
- * gets full game data from all games stored in db
- * would eventually want to likely add some filtering
- *  */
+ * Gets the full game data from all games stored in 
+ * db. Here you would eventually want to add some filtering
+ * if dealing with more than two games
+ *
+ * This would also be where I'd impliment a caching layer by
+ */
 router.get(`/`, async (req, res) => {
   const games = await BaseballGame.find({}).catch(ex => {
     return Logger.log(ex)
@@ -22,7 +24,7 @@ router.get(`/`, async (req, res) => {
 })
 
 /**
- * takes a mongoDB _id for a baseball game and returns that game
+ * Takes a mongoDB _id for a baseball game and returns that game
  */
 router.get(`/:_id`, async (req, res) => {
   let gameData = await BaseballGame.findById(req.params._id).catch(ex => {
@@ -31,22 +33,16 @@ router.get(`/:_id`, async (req, res) => {
   if (!gameData) {
     gameData = Logger.log(`Game ${req.params._id} not found;`)
   }
-  if (gameData.error) {
-    res.send(gameData)
-    return
+  if (!gameData.error) {
+    let game = await GetGameFeed(gameData.url)
+
+    if (game.error) {
+      gameData = game // game is the error object we want to return
+    } else if (game.modifiedAt > gameData.modifiedAt) {
+      gameData = await RefreshData(game, gameData.url)
+    }
   }
-
-  let game = await GetGameFeed(gameData.url)
-
-  if (game.error) {
-    gameData = game
-  } else if (game.modifiedAt > gameData.modifiedAt) {
-    gameData = await RefreshData(game, gameData.url)
-  }
-
   res.send(gameData)
-
-  // if there isn't a game object, the data is the game object
 })
 
 module.exports = router
